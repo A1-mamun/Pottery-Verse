393559527a93d3e82dff74eff50f634c6f4519ae
A1-mamun <1154almamun@gmail.com>
2024-04-27 23:55:10 +0600

    Navbar created

---
+++ src/Providers/AuthProviders.jsx
@@ -0,0 +1,76 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import app from "../firebase/firebase.config";
+import { GithubAuthProvider, GoogleAuthProvider } from "firebase/auth/cordova";
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+const AuthProviders = ({ children }) => {
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const profileUpdate = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
+  const signInGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+  const singInGithub = () => {
+    setLoading(true);
+    return signInWithPopup(auth, githubProvider);
+  };
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("auth state", currentUser);
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signIn,
+    signInGoogle,
+    singInGithub,
+    logOut,
+    profileUpdate,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProviders;
--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -1,7 +1,80 @@
+import { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../../Providers/AuthProviders";
+
 const Navbar = () => {
+  // const { user, logOut } = useContext(AuthContext);
+
+  // const handleSignOut = () => {
+  //   logOut().then().catch();
+  // };
+
+  const navlinks = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/update-profile">Update Profile</NavLink>
+      </li>
+      <li>
+        <NavLink to="/offer">Offer</NavLink>
+      </li>
+    </>
+  );
+
   return (
-    <div>
-      <h2 className="text-3xl">This is Navbar</h2>
+    <div className="navbar bg-base-100 mt-10">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[10] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navlinks}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">HavenHideaway</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navlinks}</ul>
+      </div>
+      {/* <div className="navbar-end">
+        {user && (
+          <div className=" tooltip mr-3" data-tip="tool-tip">
+            <img
+              className="btn btn-circle"
+              alt="Profile picture"
+              src={user.photoURL}
+            />
+          </div>
+        )}
+        {user ? (
+          <button onClick={handleSignOut} className="btn">
+            Sign Out
+          </button>
+        ) : (
+          <Link to="/login">
+            <button className="btn">Login</button>
+          </Link>
+        )}
+      </div> */}
     </div>
   );
 };
